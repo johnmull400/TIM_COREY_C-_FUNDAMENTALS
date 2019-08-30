b0ca2032c95db1f5e5666b3ce964a4ad3d085fe9
John Mulligan <Johnmull33@yahoo.co.uk>
2019-08-30 14:54:50 +0100

    Update 30-08-2019

---
+++ Modules/Module 2/Module02Lesson09ForLoopWithSplit/ConsoleUI/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] firstNames = new string[5];
+
+            Console.WriteLine("Please enter a comma separated list of first names.");
+            string inputNames = Console.ReadLine().ToLower();
+
+            firstNames = inputNames.Split(',');
+
+            for (int i = 0; i < firstNames.Length; i++)
+            {
+                Console.WriteLine($"The name at element {i} in the array is {firstNames[i]}");
+            }
+
+            Console.WriteLine("Applicaton End.");
+
+            Console.ReadLine(); 
+
+
+        }
+    }
+}
