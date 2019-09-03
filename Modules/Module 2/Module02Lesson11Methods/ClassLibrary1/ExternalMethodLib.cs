4c46cb175e6465c2b62787c1d4f8e2968ad59a7b
John Mulligan <Johnmull33@yahoo.co.uk>
2019-09-03 08:25:41 +0100

    Update 03-09-2019 with Methods Lesson

---
+++ Modules/Module 2/Module02Lesson11Methods/ClassLibrary1/ExternalMethodLib.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public static class ExternalMethodLib
+    {
+        public static void AskUserName(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static string GetUserName()
+        {
+
+            string output = Console.ReadLine();
+            return output;
+        }
+
+        public static void GreetUser(string inputName)
+        {
+
+            Console.WriteLine($"Hello, {inputName}");
+
+        }
+
+    }
+}
---
+++ Modules/Module 2/Module02Lesson11Methods/ConsoleUI/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //ExternalMethodLib MyLib = new ExternalMethodLib();
+            
+            //Ask the users name
+            ExternalMethodLib.AskUserName("Please enter your name");
+
+            //Get the user's name
+            string name = ExternalMethodLib.GetUserName();
+
+            //Greet the user by their name
+            ExternalMethodLib.GreetUser(name);
+
+            Console.ReadLine();
+
+            
+        }
+
+       
+
+    }
+}
